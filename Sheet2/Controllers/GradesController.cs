560242ae8de27f6d83bc19de843abf332e4c0dd3
Dylan <dylanpita007@users.noreply.github.com>
2019-09-08 12:32:13 -0400

    Add project files.

---
+++ Sheet2/Controllers/GradesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet2.Controllers
+{
+    public class GradesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            var average = 0.0;
+
+            foreach (var key in formCollection.Keys)
+            {
+                try
+                {   
+                    if (Convert.ToDouble(formCollection[key.ToString()]) < 0 ||
+                        Convert.ToDouble(formCollection[key.ToString()]) > 100)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        average += Convert.ToDouble(formCollection[key.ToString()]);
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    ViewData["message"] = "Enter numbers ranging from 0 - 100.";
+                    return View();
+                }
+            }
+
+            average = average / formCollection.Count;
+
+            ViewData["average"] = average;
+
+            return View();
+        }
+    }
+}
---
+++ Sheet2/Controllers/LoanController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet2.Controllers
+{
+    public class LoanController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Double initialInvestment, Double interest, int numYears)
+        {
+            Double finalInvestment = initialInvestment;
+
+            for (int i = 0; i < numYears; i++)
+            {
+                finalInvestment += finalInvestment * (interest / 100);
+            }
+
+            ViewData["initialInvestment"] = initialInvestment;
+            ViewData["interest"] = interest;
+            ViewData["numYears"] = numYears;
+
+            ViewData["finalInvestment"] = Math.Round(finalInvestment, 2);
+            return View();
+        }
+    }
+}
